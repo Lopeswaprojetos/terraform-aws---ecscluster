1c5719d75324e51c497c85fa090e5de2a9512664
Wagner Lopes <161225187+Lopeswaprojetos@users.noreply.github.com>
2024-03-26 18:49:19 -0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
 output "vpc_id" {
-  value = aws_vpc.main.id
+  value = aws_vpc.central-vpc.id
 }
 
 output "public_subnet_a_id" {
-  value = aws_subnet.public_subnet_a.id
+  value = aws_subnet.sub-rede-publica_a.id
 }
 
 output "public_subnet_b_id" {
-  value = aws_subnet.public_subnet_b.id
+  value = aws_subnet.sub-rede-publica_b.id
 }
 
 output "ecs_cluster_name" {
-  value = aws_ecs_cluster.main.name
+  value = aws_ecs_cluster.meu-cluster-ecs.name
 }
 
 output "load_balancer_dns_name" {
-  value = aws_lb.web.dns_name
+  value = aws_lb.meu-alb.dns_name
 }
