aa34cc971ee9dd056ec9434042955c1cb50ea856
Wagner Lopes <161225187+Lopeswaprojetos@users.noreply.github.com>
2024-03-26 18:39:54 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,44 +2,44 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_vpc" "main" {
+resource "aws_vpc" "central-vpc" {
   cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_subnet" "public_subnet_a" {
+resource "aws_subnet" "sub-rede-publica_a" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
 }
 
-resource "aws_subnet" "public_subnet_b" {
+resource "aws_subnet" "sub-rede-publica_b" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "us-east-1b"
 }
 
-resource "aws_internet_gateway" "gw" {
+resource "aws_internet_gateway" "internet-gateway" {
   vpc_id = aws_vpc.main.id
 }
 
-resource "aws_vpc_attachment" "gw_attach" {
+resource "aws_vpc_attachment" "anexo-vpc-internet-gateway" {
   vpc_id             = aws_vpc.main.id
   internet_gateway_id = aws_internet_gateway.gw.id
 }
 
-resource "aws_ecs_cluster" "main" {
-  name = "my-ecs-cluster"
+resource "aws_ecs_cluster" "meu-cluster-ecs" {
+  name = "meu-cluster-ecs"
 }
 
-resource "aws_lb" "web" {
-  name               = "my-alb"
+resource "aws_lb" "meu-alb" {
+  name               = "meu-alb"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.lb.id]
   subnets            = [aws_subnet.public_subnet_a.id, aws_subnet.public_subnet_b.id]
 }
 
-resource "aws_security_group" "lb" {
+resource "aws_security_group" "lb-sg" {
   name        = "lb-sg"
   description = "Security group for ALB"
   vpc_id      = aws_vpc.main.id
